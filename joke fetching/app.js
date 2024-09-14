48fd22072442f1f114fcc93173a225d720bd2352
ASHIKUR RAHMAN <ashikariyan503@gmail.com>
2024-09-14 20:11:22 +0600

    Added catch method to check error

--- joke fetching/app.js
+++ joke fetching/app.js
@@ -9,6 +9,9 @@ function getJoke(){
     
     axios.get('https://icanhazdadjoke.com', config).then((res)=>{
         document.getElementById('joke').innerHTML = res.data.joke;
+    }).catch((err)=>{
+        console.log(err);
+        document.getElementById('joke').innerHTML = "Something Went wrong."
     })
 
 }
